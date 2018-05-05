a171f108e04d74fde34823ffebe03e75dd62014b
Lance Vance <okiki.oyelaw@gmail.com>
2018-05-05 12:27:25 +0100

    setup environment variables using convict

--- src/persistence/index.test.js
+++ src/persistence/index.test.js
@@ -1,15 +1,19 @@
 import mysqlDb, { close } from './';
 
+import config from '../config';
+
+// console.log(config.get('mysql'));
+
 describe('mysqlDb', () => {
   describe('.connect', () => {
     it('connects to mysql database', async () => {
       const db = mysqlDb({
         client: 'mysql',
         connection: {
-          host: 'localhost',
-          user: 'root',
-          password: 'root',
-          database: 'mysql',
+          host: config.get('mysql.host'),
+          user: config.get('mysql.user'),
+          password: config.get('mysql.pass'),
+          database: config.get('mysql.database'),
         }
       });
       const actual = await db.schema.hasTable('proc');
