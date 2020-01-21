ea986793477712bcaa2dac529189c2e9ed61b844
Ehbraheem <ehbraheem@gmail.com>
2020-01-21 12:52:19 +0100

    Fixed error when dbConnectOption is omitted

--- src/persistence/mongoose/connect.js
+++ src/persistence/mongoose/connect.js
@@ -1,8 +1,11 @@
 export default async (Mongoose, config) => {
   try {
+    const dbConnectOptions = config.has('dbConnectOptions') ? {
+      ...config.get('dbConnectOptions'), ...{ useNewUrlParser: true }
+    } : { useNewUrlParser: true }
     return await Mongoose.connect(`${config.get('mongo.host')}`, {
       ...{ dbName: config.get('mongo.database') },
-      ...config.get('dbConnectOptions')
+      ...dbConnectOptions
     });
   } catch(error) {
     throw error;
