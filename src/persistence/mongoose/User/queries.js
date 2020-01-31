40cc08105259fc634ab728b7e7b60f0e663886fd
Micah J <justmic007>
2020-01-31 12:32:14 +0100

    find users by id

--- src/persistence/mongoose/User/queries.js
+++ src/persistence/mongoose/User/queries.js
@@ -12,6 +12,10 @@ const create = async ({ payload }) => {
   return await user.save();
 };
 
+const findById = async ({ payload }) => User.findOne({ uuid: payload.uuid, 'meta.active': { $gte: true } });
+
+
 export default client => ({
   create: async ({ payload, config }) => await create({ client, payload, config }),
+  findById: async ({ payload }) => findById({ payload })
 });
