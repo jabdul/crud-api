36c329b089f72c4e80ac4a685c4218932c82e76d
Ehbraheem <ehbraheem@gmail.com>
2020-02-03 17:01:24 +0100

    Added meta tag to user

--- test/factories/user.js
+++ test/factories/user.js
@@ -1,8 +1,10 @@
 import { factory } from 'factory-girl';
 
 import User from '../../src/persistence/mongoose/User/model'
+import Meta from './meta'; // eslint-disable-line no-unused-vars
 
 export default factory.define('User', User, {
   firstname: 'test',
-  lastname: 'Test'
+  lastname: 'Test',
+  meta: factory.assocAttrs('Meta')
 });
