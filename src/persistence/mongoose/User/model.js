37fd66d368b0b9c4275f7000e0cf5727b22dbe50
Ehbraheem <ehbraheem@gmail.com>
2020-02-03 16:35:35 +0100

    Added meta field

--- src/persistence/mongoose/User/model.js
+++ src/persistence/mongoose/User/model.js
@@ -3,11 +3,21 @@ import uuid from 'uuid';
 
 const Schema = mongoose.Schema;
 
+export const MetaSchema = new Schema({
+  active: { type: Boolean, default: true },
+  updated: { type: Date },
+  created: { type: Date },
+},
+  { _id: false },
+);
+
+
 const UserSchema = new Schema({
- uuid: { type: String, default: uuid.v4 },
- firstname: { type: String, required: true },
- lastname: { type: String, required: true },
- created: { type: Date, default: Date.now }
+  uuid: { type: String, default: uuid.v4 },
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
+  created: { type: Date, default: Date.now },
+  meta: { type: MetaSchema },
 });
 
 const User = mongoose.model('Users', UserSchema);
