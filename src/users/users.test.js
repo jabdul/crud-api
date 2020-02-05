3799c14286ff34261f6202c11fe5fdf8ec85d1b8
Ehbraheem <ehbraheem@gmail.com>
2020-02-05 13:06:22 +0100

    Completed happy and sad path test

--- src/users/users.test.js
+++ src/users/users.test.js
@@ -28,8 +28,28 @@ describe('Users', () => {
       expect(response.statusMessage).toBe('Created');
       expect(response.headers['content-type']).toEqual('application/hal+json');
 
-      const body = parsedResponse(response)
-      console.log(body)// eslint-disable-line no-console
+      const user = parsedResponse(response)
+      expect(user).toHaveProperty('firstname')
+      expect(user).toHaveProperty('lastname')
+      expect(user).toHaveProperty('uuid')
+      expect(user['firstname']).toEqual(payload['firstname'])
+      expect(user['lastname']).toEqual(payload['lastname'])
+      expect(user['uuid']).toMatch(
+        /\b(?=([0-9A-F]{8})\b)\1-(?=([0-9A-F]{4}))\2-(?=(4[0-9A-F]{3}))\3-(?=([89AB][0-9A-F]{3}))\4-(?=([0-9A-F]{12}))\5\b/i // eslint-disable-line
+      )
     })
-  })
+
+    it('Cannot create User', async () => {
+      const payload = await factory.attrs('User', { firstname: null });
+      const response = await app.inject({
+        method: 'POST',
+        url,
+        payload
+      });
+
+      expect(response.statusCode).toBe(400);
+      expect(response.statusMessage).toBe('Bad Request');
+      expect(response.headers['content-type']).toEqual('application/json; charset=utf-8');
+    });
+  });
 });
