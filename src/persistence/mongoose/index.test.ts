4719994ed012d7ef814ace72d144f1770ab52713
Micah J <justmic007>
2020-02-26 13:01:12 +0100

    renamed all .js extention files in /src to .ts

---
+++ src/persistence/mongoose/index.test.ts
@@ -0,0 +1,15 @@
+import Mongoose from "mongoose";
+
+import dbConnect, { close } from ".";
+import config from "../../config";
+
+describe("mongoose", () => {
+  describe(".connect", () => {
+    it.skip("connects to mongodb", async () => {
+      const mongoose = await dbConnect(config);
+
+      expect(mongoose).toEqual(Mongoose);
+      close(mongoose);
+    });
+  });
+});
