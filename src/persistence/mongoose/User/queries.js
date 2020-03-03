fbe03e31790563233c018cdb2d4bea3a89836fb0
justmic007 <36313854+justmic007@users.noreply.github.com>
2020-03-03 09:56:18 +0100

    Findone test1 (#40)
    * find users by id
    
    * rebase branch with master
    
    * refactored sad test case for findById
    
    * refactored sad test case for findById
    
    * happy part of updateById passes
    
    * test sad case for updateById
    
    * refactored findById and updateById test
    
    * removed console.logs
    
    * find user updated
    
    * restore dbcleaner
    
    * pass in payload needed to update
    
    * restored dbcleaner
    
    * refactored updateById test code
    
    * refactored test
    
    * build src to lib
    
    * utility function for findById
    
    * remove logs
    
    * add to util function
    
    * utility function for updateById
    
    * refactored dbCleaner
    
    * remove logs

--- src/persistence/mongoose/User/queries.js
+++ src/persistence/mongoose/User/queries.js
@@ -1,3 +1,5 @@
+import { pickBy, omit } from 'ramda';
+
 import User from './model';
 
 const create = async ({ payload }) => {
@@ -12,6 +14,22 @@ const create = async ({ payload }) => {
   return await user.save();
 };
 
+const findById = async ({ payload }) => User.findOne({ uuid: payload.uuid });
+
+const updateById = async ({ payload }) => User.updateOne(
+  { uuid: payload.uuid },
+  {
+    $set: {
+      ...pickBy(val => val !== undefined, {
+        ...omit('uuid', payload)
+      }),
+      'meta.updated': Date.now()
+    }
+  }
+);
+
 export default client => ({
   create: async ({ payload, config }) => await create({ client, payload, config }),
+  findById: async ({ payload, config }) => await findById({ client, payload, config }),
+  updateById: async ({ payload, config }) => await updateById({ client, payload, config })
 });
--- src/persistence/mongoose/User/queries.test.js
+++ src/persistence/mongoose/User/queries.test.js
@@ -8,6 +8,19 @@ import { mongooseConnect, dbConfig as config } from '../../../index';
 const db = mongooseConnect(config);
 let userQueries = queries(db); // eslint-disable-line no-unused-vars
 
+const verifyUser = user => {
+  expect(user).toHaveProperty('firstname');
+  expect(user).toHaveProperty('lastname');
+  expect(user).toHaveProperty('uuid');
+};
+
+const verifyResponse = ({ user, payload }) => {
+  expect(user['firstname']).toEqual(payload['firstname']);
+  expect(user['lastname']).toEqual(payload['lastname']);
+  expect(user['uuid']).toEqual(payload['uuid']);
+  expect(user['meta.active']).toEqual(payload['meta.active']);
+};
+
 describe('Users queries', () => {
 
   describe('create', () => {
@@ -42,4 +55,59 @@ describe('Users queries', () => {
 
     })
   })
+
+  describe('findById', () => {
+    it('can find a single user', async () => {
+      const user = await factory.create('User');
+
+      const findUser = await userQueries.findById({ payload: { uuid: user.uuid } });
+
+      expect(findUser).toBeDefined();
+      verifyUser(findUser);
+
+      verifyResponse({ user: findUser, payload: user })
+    });
+
+    it('should not find a user with an invalid uuid', async () => {
+      const uuid = '02117187-a5d5-4681-b087-8b4b337d5b8d';
+      const res = await userQueries.findById({ payload: { uuid: uuid } });
+      expect(res).toBe(null);
+
+    })
+  })
+
+  describe('updateById', () => {
+    it('update a single user', async () => {
+      const user = await factory.create('User');
+      const updateUser = await userQueries.updateById({ payload: { uuid: user.uuid, firstname: 'Micah', lastname: 'Joel' } });
+
+      expect(updateUser).toBeDefined()
+      expect(updateUser).toHaveProperty('nModified');
+      expect(updateUser).toHaveProperty('n');
+      expect(updateUser).toHaveProperty('ok');
+      expect(updateUser['nModified']).toEqual(1);
+      expect(updateUser['n']).toEqual(1);
+      expect(updateUser['ok']).toEqual(1);
+
+      const findOneUser = await userQueries.findById({ payload: { uuid: user.uuid } });
+
+      expect(findOneUser).toBeDefined();
+      verifyUser(findOneUser);
+      expect(findOneUser.meta).toHaveProperty('updated');
+    });
+
+    it('should not update a user with an invalid uuid', async () => {
+
+      const uuid = '84b5eaa1-bcb0-4107-a082-75156bb6c56a';
+
+      const res = await userQueries.updateById({ payload: { uuid, firstname: 'Wendy', lastname: 'Mcmanaman' } });
+
+      expect(res.meta).toBeUndefined()
+
+      const findOneUser = await userQueries.findById({ payload: { uuid: uuid } });
+
+      expect(findOneUser).toBeNull()
+    })
+  }
+  )
 });
