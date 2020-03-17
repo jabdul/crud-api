b16b5338d54e1f78801b635b0d29544d65251e3b
Ehbraheem <ehbraheem@gmail.com>
2020-03-17 14:28:16 +0100

    Fixed queries test teardown

--- src/persistence/mongoose/index.js
+++ src/persistence/mongoose/index.js
@@ -8,7 +8,7 @@ export const schema = client => queries(client);
 export const close = mongo => mongo.disconnect();
 
 export default async config => {
-  const client = connect(new mongoose.Mongoose(), config);
+  const client = await connect(new mongoose.Mongoose(), config);
   mongoose.set('useCreateIndex', true);
   mongoose.set('bufferCommands', false);
   
