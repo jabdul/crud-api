aff4ebc794a3e4936fdc59f16563fa152b5a32b3
Ehbraheem <ehbraheem@gmail.com>
2022-01-27 21:45:11 +0100

    Build new changes

--- lib/config.d.ts
+++ lib/config.d.ts
@@ -1,147 +1,147 @@
 import convict, { Config } from 'convict';
 export declare const conf: {
-  mongo: {
-    host: {
-      doc: string;
-      format: StringConstructor;
-      default: string;
-      env: string;
-      arg: string;
+    mongo: {
+        host: {
+            doc: string;
+            format: StringConstructor;
+            default: string;
+            env: string;
+            arg: string;
+        };
+        database: {
+            doc: string;
+            format: StringConstructor;
+            default: string;
+            env: string;
+            arg: string;
+        };
+        user: {
+            doc: string;
+            format: StringConstructor;
+            default: string;
+            env: string;
+            arg: string;
+        };
+        pass: {
+            doc: string;
+            format: StringConstructor;
+            default: string;
+            env: string;
+            arg: string;
+        };
     };
-    database: {
-      doc: string;
-      format: StringConstructor;
-      default: string;
-      env: string;
-      arg: string;
+    env: {
+        doc: string;
+        format: string[];
+        default: string;
+        env: string;
     };
-    user: {
-      doc: string;
-      format: StringConstructor;
-      default: string;
-      env: string;
-      arg: string;
-    };
-    pass: {
-      doc: string;
-      format: StringConstructor;
-      default: string;
-      env: string;
-      arg: string;
-    };
-  };
-  env: {
-    doc: string;
-    format: string[];
-    default: string;
-    env: string;
-  };
-  server: {
-    hostname: {
-      doc: string;
-      format: StringConstructor;
-      default: string;
-      env: string;
-      arg: string;
-    };
-    port: {
-      doc: string;
-      format: string;
-      default: number;
-      env: string;
-      arg: string;
-    };
-    tlsCert: {
-      doc: string;
-      format: StringConstructor;
-      default: string;
-      env: string;
-      arg: string;
-    };
-    tlsKey: {
-      doc: string;
-      format: StringConstructor;
-      default: string;
-      env: string;
-      arg: string;
-    };
-    tlsCa: {
-      doc: string;
-      format: StringConstructor;
-      default: string;
-      env: string;
-      arg: string;
-    };
-    secure: {
-      doc: string;
-      format: BooleanConstructor;
-      default: boolean;
-      env: string;
-      arg: string;
+    server: {
+        hostname: {
+            doc: string;
+            format: StringConstructor;
+            default: string;
+            env: string;
+            arg: string;
+        };
+        port: {
+            doc: string;
+            format: string;
+            default: number;
+            env: string;
+            arg: string;
+        };
+        tlsCert: {
+            doc: string;
+            format: StringConstructor;
+            default: string;
+            env: string;
+            arg: string;
+        };
+        tlsKey: {
+            doc: string;
+            format: StringConstructor;
+            default: string;
+            env: string;
+            arg: string;
+        };
+        tlsCa: {
+            doc: string;
+            format: StringConstructor;
+            default: string;
+            env: string;
+            arg: string;
+        };
+        secure: {
+            doc: string;
+            format: BooleanConstructor;
+            default: boolean;
+            env: string;
+            arg: string;
+        };
+        db: {
+            doc: string;
+            format: StringConstructor;
+            default: string;
+            env: string;
+            arg: string;
+        };
     };
     db: {
-      doc: string;
-      format: StringConstructor;
-      default: string;
-      env: string;
-      arg: string;
+        dump: {
+            doc: string;
+            format: StringConstructor;
+            default: string;
+            env: string;
+            arg: string;
+        };
     };
-  };
-  db: {
-    dump: {
-      doc: string;
-      format: StringConstructor;
-      default: string;
-      env: string;
-      arg: string;
+    mysql: {
+        host: {
+            doc: string;
+            format: StringConstructor;
+            default: string;
+            env: string;
+            arg: string;
+        };
+        database: {
+            doc: string;
+            format: StringConstructor;
+            default: string;
+            env: string;
+            arg: string;
+        };
+        user: {
+            doc: string;
+            format: StringConstructor;
+            default: string;
+            env: string;
+            arg: string;
+        };
+        pass: {
+            doc: string;
+            format: StringConstructor;
+            default: string;
+            env: string;
+            arg: string;
+        };
     };
-  };
-  mysql: {
-    host: {
-      doc: string;
-      format: StringConstructor;
-      default: string;
-      env: string;
-      arg: string;
+    jwt: {
+        secret: {
+            doc: string;
+            format: StringConstructor;
+            default: string;
+            env: string;
+            arg: string;
+        };
+        expires: {
+            doc: string;
+            format: NumberConstructor;
+            default: number;
+            env: string;
+            arg: string;
+        };
     };
-    database: {
-      doc: string;
-      format: StringConstructor;
-      default: string;
-      env: string;
-      arg: string;
-    };
-    user: {
-      doc: string;
-      format: StringConstructor;
-      default: string;
-      env: string;
-      arg: string;
-    };
-    pass: {
-      doc: string;
-      format: StringConstructor;
-      default: string;
-      env: string;
-      arg: string;
-    };
-  };
-  jwt: {
-    secret: {
-      doc: string;
-      format: StringConstructor;
-      default: string;
-      env: string;
-      arg: string;
-    };
-    expires: {
-      doc: string;
-      format: NumberConstructor;
-      default: number;
-      env: string;
-      arg: string;
-    };
-  };
 };
 declare const loadConfig: (appConfig: {}, configFiles: any, options?: {}) => Config<object>;
 export declare const dbConfig: convict.Config<object>;
--- lib/server.d.ts
+++ lib/server.d.ts
@@ -1,17 +1,5 @@
 import Hapi from '@hapi/hapi';
 import { ServerArgs } from './';
-declare const _default: ({
-  dbConnect,
-  schema,
-  config,
-  routes,
-  services,
-  plugins,
-  postRegisterHook,
-  swaggerOptions,
-  swaggerUiOptions,
-  loggerOptions,
-  serverOptions,
-}: ServerArgs) => Promise<Hapi.Server>;
+declare const _default: ({ dbConnect, schema, config, routes, services, plugins, postRegisterHook, swaggerOptions, swaggerUiOptions, loggerOptions, serverOptions, }: ServerArgs) => Promise<Hapi.Server>;
 export default _default;
 //# sourceMappingURL=server.d.ts.map
