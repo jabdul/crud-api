068faad693d8a4f7893391b92848cd9a0f1f74f5
Ehbraheem <ehbraheem@gmail.com>
2022-01-27 21:45:11 +0100

    Build new changes

--- lib/server.d.ts
+++ lib/server.d.ts
@@ -1,16 +1,4 @@
 import { ServerArgs } from './';
-declare const _default: ({
-  dbConnect,
-  schema,
-  config,
-  routes,
-  services,
-  plugins,
-  postRegisterHook,
-  swaggerOptions,
-  swaggerUiOptions,
-  loggerOptions,
-  serverOptions,
-}: ServerArgs) => Promise<any>;
+declare const _default: ({ dbConnect, schema, config, routes, services, plugins, postRegisterHook, swaggerOptions, swaggerUiOptions, loggerOptions, serverOptions, }: ServerArgs) => Promise<any>;
 export default _default;
 //# sourceMappingURL=server.d.ts.map
