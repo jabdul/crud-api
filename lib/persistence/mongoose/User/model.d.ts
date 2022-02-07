c927bf3137267baac7954d3fa80ae182e4fb0099
Ehbraheem <ehbraheem@gmail.com>
2022-02-07 20:43:52 +0100

    Remove unnecessary files

--- lib/persistence/mongoose/User/model.d.ts
+++ lib/persistence/mongoose/User/model.d.ts
@@ -1,17 +1,16 @@
 import mongoose, { Document, Schema } from 'mongoose';
 interface MetaFields {
-    active: boolean;
-    updated?: Date;
-    created?: Date;
+  active: boolean;
+  updated?: Date;
+  created?: Date;
 }
 interface UserFields {
-    uuid?: string;
-    firstname: string;
-    lastname: string;
-    meta: MetaFields;
+  uuid?: string;
+  firstname: string;
+  lastname: string;
+  meta: MetaFields;
 }
-export interface UserI extends UserFields, Document {
-}
+export interface UserI extends UserFields, Document {}
 export declare const MetaSchema: Schema;
 declare type User = mongoose.Model<UserI>;
 declare const User: mongoose.Model<UserI, {}, {}, {}>;
