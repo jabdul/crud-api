6bd0b82673341939c386a808aae0d7ff014ed57e
Ehbraheem <ehbraheem@gmail.com>
2022-02-07 20:43:52 +0100

    Remove unnecessary files

--- lib/server.d.ts
+++ lib/server.d.ts
@@ -1,9 +1,20 @@
 import Hapi from '@hapi/hapi';
 import { DbClient, Dict, ServerArgs } from './';
 declare type ServerType = Hapi.Server & {
-    db?: DbClient;
-    schema?: Dict;
+  db?: DbClient;
+  schema?: Dict;
 };
-declare const _default: ({ dbConnect, schema, config, routes, services, plugins, postRegisterHook, swaggerOptions, loggerOptions, serverOptions, }: ServerArgs) => Promise<ServerType>;
+declare const _default: ({
+  dbConnect,
+  schema,
+  config,
+  routes,
+  services,
+  plugins,
+  postRegisterHook,
+  swaggerOptions,
+  loggerOptions,
+  serverOptions,
+}: ServerArgs) => Promise<ServerType>;
 export default _default;
 //# sourceMappingURL=server.d.ts.map
