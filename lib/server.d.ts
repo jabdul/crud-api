2be52a4f766e367d22ba903d8ebe626f68f8c5a4
Ehbraheem <ehbraheem@gmail.com>
2022-04-22 17:11:12 +0100

    Release new version

--- lib/server.d.ts
+++ lib/server.d.ts
@@ -1,4 +1,15 @@
 import { CrudServer, ServerArgs } from './';
-declare const _default: ({ dbConnect, schema, config, routes, services, plugins, postRegisterHook, swaggerOptions, loggerOptions, serverOptions, }: ServerArgs) => Promise<CrudServer>;
+declare const _default: ({
+  dbConnect,
+  schema,
+  config,
+  routes,
+  services,
+  plugins,
+  postRegisterHook,
+  swaggerOptions,
+  loggerOptions,
+  serverOptions,
+}: ServerArgs) => Promise<CrudServer>;
 export default _default;
 //# sourceMappingURL=server.d.ts.map
