fe8cfb61edfae24aaee0a9931cbfe4e150e50763
Ehbraheem <ehbraheem@gmail.com>
2022-04-24 18:18:21 +0100

    Ignore create collection

--- lib/persistence/mongoose/createCollection.d.ts
+++
@@ -1,4 +0,0 @@
-import { Mongoose } from 'mongoose';
-declare const _default: (dbConnection: Mongoose) => Promise<Mongoose>;
-export default _default;
-//# sourceMappingURL=createCollection.d.ts.map
--- src/persistence/mongoose/index.ts
+++ src/persistence/mongoose/index.ts
@@ -1,7 +1,6 @@
 import mongoose, { Mongoose } from 'mongoose';
 
 import connect from './connect';
-import createCollection from './createCollection';
 import createIndex from './createIndex';
 import queries from './queries';
 import { DbClient, Dict } from '../../';
@@ -16,4 +15,4 @@ mongoose.set('bufferCommands', false);
 const pipePromiseFns = (...fns) => x => fns.reduce((y, f) => y.then(f), Promise.resolve(x));
 
 export default (config: Config<object>): Promise<Mongoose> =>
-  pipePromiseFns(connect(config), createCollection, createIndex(config))(mongoose);
+  pipePromiseFns(connect(config), createIndex(config))(mongoose);
