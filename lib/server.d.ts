16b1a92e18f13a89055446053d414caf314dde78
Ehbraheem <ehbraheem@gmail.com>
2023-03-07 16:46:24 +0100

    Allow user specified config object

--- lib/server.d.ts
+++ lib/server.d.ts
@@ -1,4 +1,17 @@
 import { CrudServer, ServerArgs } from './';
-declare const _default: ({ dbConnect, schema, config, routes, services, plugins, postRegisterHook, swaggerOptions, loggerOptions, serverOptions, dockerized, intializers, }: ServerArgs) => Promise<CrudServer>;
+declare const _default: ({
+  dbConnect,
+  schema,
+  config,
+  routes,
+  services,
+  plugins,
+  postRegisterHook,
+  swaggerOptions,
+  loggerOptions,
+  serverOptions,
+  dockerized,
+  intializers,
+}: ServerArgs) => Promise<CrudServer>;
 export default _default;
 //# sourceMappingURL=server.d.ts.map
